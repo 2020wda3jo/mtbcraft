9803916444b282b14600132a9032418002a88f83
2020wda3jo <61041539+2020wda3jo@users.noreply.github.com>
2020-04-10 23:10:35 +0900

    spring

---
+++ mtbcraft/src/main/java/com/spring/dec30/MtbcraftApplication.java
@@ -0,0 +1,13 @@
+package com.spring.dec30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtbcraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtbcraftApplication.class, args);
+	}
+
+}
