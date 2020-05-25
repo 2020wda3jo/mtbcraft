228a9525a28640576a3f0b4c7556c798575993ea
2020wda3jo <61041539+2020wda3jo@users.noreply.github.com>
2020-05-25 19:27:06 +0900

    한글

--- spring/mtbcraft/src/main/java/com/mtbcraft/config/SecurityConfig.java
+++ spring/mtbcraft/src/main/java/com/mtbcraft/config/SecurityConfig.java
@@ -16,13 +16,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
-		.antMatchers("/**").permitAll() // 紐⑤뱺 沅뚰븳�쓣 以�.=濡쒓렇�씤 �븘�슂 �뾾�쓬.
-		.antMatchers("/files/**").hasRole("USER") // user 沅뚰븳留� �젒洹� 媛��뒫.
+		.antMatchers("/**").permitAll() // 모든 권한을 줌.=로그인 필요 없음.
+		.antMatchers("/files/**").hasRole("USER") // user 권한만 접근 가능.
 		.antMatchers("/logout").permitAll()
-		.anyRequest().authenticated() // 濡쒓렇�씤 泥댄겕�븿.
+		.anyRequest().authenticated() // 로그인 체크함.
 		.and()
 		.formLogin()
-		.loginPage("/login") //�씠 以꾩쓣 吏��슦硫� �뒪�봽留곸씠 �젣怨듯븯�뒗 �뤌�씠 異쒕젰�맖.
+		.loginPage("/login") //이 줄을 지우면 스프링이 제공하는 폼이 출력됨.
 		.permitAll()
 		.successHandler(successHandler())
 		.and()
@@ -32,10 +32,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.invalidateHttpSession(true)
 		.and()
 		.csrf();
-		//api�씠�썑�쓽 二쇱냼�뱾�� csrf�넗�겙寃��궗瑜� �뙣�벐�븳�떎
+		//api이후의 주소들은 csrf토큰검사를 패쓰한다
 		http.csrf().ignoringAntMatchers("/api/**");
 		http.csrf().ignoringAntMatchers("/riding/**");
-		http.csrf().ignoringAntMatchers("/androidUpload/**");
 		//.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 		
 		}
@@ -48,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
     public void configure(WebSecurity web) throws Exception
     {
-        // static �뵒�젆�꽣由ъ쓽 �븯�쐞 �뙆�씪 紐⑸줉�� �씤利� 臾댁떆 ( = �빆�긽�넻怨� )
+        // static 디렉터리의 하위 파일 목록은 인증 무시 ( = 항상통과 )
         web.ignoring().antMatchers("/css/**", "/js/**", "/imgs/**");
     }
 }
--- spring/mtbcraft/src/main/java/com/mtbcraft/controller/AndroidController.java
+++ spring/mtbcraft/src/main/java/com/mtbcraft/controller/AndroidController.java
@@ -1,23 +1,14 @@
 package com.mtbcraft.controller;
 
-import java.io.File;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import java.util.*;
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.mtbcraft.dto.RidingRecord;
 import com.mtbcraft.service.AndroidService;
 import com.mtbcraft.service.MemberService;
@@ -31,13 +22,13 @@ public class AndroidController {
 	@Autowired
 	AndroidService androidService;
 
-	// 二쇳뻾湲곕줉 �벑濡�(�븞�뱶濡쒖씠�뱶)
+	// 주행기록 등록(안드로이드)
 	@RequestMapping(value = "/api/upload")
 	@ResponseBody
 	public Map<String, String> insertriding(HttpServletRequest request) throws Exception {
 		RidingRecord record = new RidingRecord();
 
-		// �쁽�옱�궇吏� timestamp
+		// 현재날짜 timestamp
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		// formatter.setTimeZone(TimeZone.getTimeZone("GMT+09"));
 		Calendar cal = Calendar.getInstance();
@@ -57,19 +48,19 @@ public class AndroidController {
 		record.setRr_area(request.getParameter("rr_area"));
 
 		androidService.insertRecord(record);
-		// �븞�뱶濡쒖씠�뱶濡쒕��꽣 諛쏆� �뜲�씠�꽣
-		System.out.println("rr_rider " + request.getParameter("rr_rider")); // �쉶�썝�븘�씠�뵒
-		System.out.println("rr_distance " + request.getParameter("rr_distance")); // �삤�뒛�궇吏�
-		System.out.println("rr_topspeed " + request.getParameter("rr_topspeed")); // �냼�슂�떆媛�
-		System.out.println("rr_avgspeed " + request.getParameter("rr_avgspeed")); // �씠�룞嫄곕━
-		System.out.println("rr_high " + request.getParameter("rr_high")); // 理쒕��냽�룄
-		System.out.println("rr_gpx " + request.getParameter("rr_gpx")); // �룊洹좎냽�룄
-		System.out.println("rr_open " + request.getParameter("rr_open")); // �쉷�뱷怨좊룄
+		// 안드로이드로부터 받은 데이터
+		System.out.println("rr_rider " + request.getParameter("rr_rider")); // 회원아이디
+		System.out.println("rr_distance " + request.getParameter("rr_distance")); // 오늘날짜
+		System.out.println("rr_topspeed " + request.getParameter("rr_topspeed")); // 소요시간
+		System.out.println("rr_avgspeed " + request.getParameter("rr_avgspeed")); // 이동거리
+		System.out.println("rr_high " + request.getParameter("rr_high")); // 최대속도
+		System.out.println("rr_gpx " + request.getParameter("rr_gpx")); // 평균속도
+		System.out.println("rr_open " + request.getParameter("rr_open")); // 획득고도
 		System.out.println("rr_breaktime " + request.getParameter("rr_breaktime")); // gpx
-		System.out.println("rr_time " + request.getParameter("rr_time")); // 怨듦컻�뿬遺�
-		System.out.println("rr_area " + request.getParameter("rr_area")); // �쉷�뱷怨좊룄
+		System.out.println("rr_time " + request.getParameter("rr_time")); // 공개여부
+		System.out.println("rr_area " + request.getParameter("rr_area")); // 획득고도
 
-		// �븞�뱶濡쒖씠�뱶�뿉寃� �쟾�떖�븯�뒗 �뜲�씠�꽣
+		// 안드로이드에게 전달하는 데이터
 		Map<String, String> result = new HashMap<String, String>();
 		result.put("data1", request.getParameter("rr_rider"));
 		result.put("data2", request.getParameter("rr_distance"));
@@ -85,48 +76,17 @@ public class AndroidController {
 		return result;
 	}
 
-	// 二쇳뻾湲곕줉 媛��졇�삤湲�
+	// 주행기록 가져오기
 	@RequestMapping(value = "/api/get/{rr_rider}")
 	public @ResponseBody List<RidingRecord> getRidingRecord(@PathVariable String rr_rider) throws Exception {
 		return memberService.getRidingRecord(rr_rider);
 	}
 
-	// 二쇳뻾湲곕줉 媛��졇�삤湲�
+	// 주행기록 가져오기
 	@RequestMapping(value = { "/api/get/{rr_rider}/{rr_num}" })
 	public @ResponseBody List<RidingRecord> getRidingRecordDetail(@PathVariable(value = "rr_rider") String rr_rider,
 			@PathVariable(value = "rr_num") String rr_num) throws Exception {
 		System.out.println(memberService.getRidingRecord(rr_rider));
 		return memberService.getRidingRecordDetail(rr_rider, rr_num);
-	}
-	
-	@RequestMapping(value="/androidUpload", method=RequestMethod.POST)
-	public String upload(HttpServletRequest request, MultipartFile file1){
-
-		try{
-				
-			String path = "";
-			String fileName="";
-				
-			if(!file1.isEmpty()){ //첨부파일이 존재하면
-				//첨부파일의 이름
-				fileName=file1.getOriginalFilename();
-				try{
-					//디렉토리 생성
-					new File(path + "/"+ fileName).mkdirs();
-					//지정된 업로드 경로로 저장됨
-					file1.transferTo(new File(path + "/"+ fileName));
-					System.out.println(path);
-
-						
-				}catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-				
-				
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		return "redirect:/";
 	}
 }
