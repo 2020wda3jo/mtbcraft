0e25e2e21cc633d8bd3f5ae8e59df1a24aa38c34
hyungtack <61041539+2020wda3jo@users.noreply.github.com>
2020-06-11 23:33:26 +0900

    Revert "커뮤니티 대강 완성"
    This reverts commit ef1eee4a89329aa167d2fd645f9d21c3274bb94a.

--- spring/mtbcraft/src/main/java/com/mtbcraft/service/CommunityService.java
+++ spring/mtbcraft/src/main/java/com/mtbcraft/service/CommunityService.java
@@ -1,7 +1,5 @@
 package com.mtbcraft.service;
 
-import java.util.List;
-
 import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,41 +9,27 @@ import org.springframework.transaction.annotation.Transactional;
 import com.mtbcraft.dto.Club;
 import com.mtbcraft.dto.Club_Join;
 import com.mtbcraft.mapper.CommunityMapper;
+import com.mtbcraft.mapper.MemberMapper;
 
 @Service
 @Transactional
 public class CommunityService {
-	@Resource(name = "com.mtbcraft.mapper.CommunityMapper")
+	@Resource(name="com.mtbcraft.mapper.CommunityMapper")
 	@Autowired
 	private CommunityMapper communityMapper;
-
-	// 클럽 생성
+	
+	//클럽 생성
 	public void insertClub(Club club) throws Exception {
 		communityMapper.insertClub(club);
 	}
-
-	/// 클럽이름 중복검사
-	public int checkClubName(String cb_name) {
+	
+	///클럽이름 중복검사
+	public int checkClubName(String cb_name){
 		return communityMapper.checkClubName(cb_name);
 	}
-
+	
 	public void insertCJ(Club_Join cb_join) {
 		communityMapper.insertCJ(cb_join);
 	}
-
-	// 클럽 멤버수 조회
-	public int getMember(Club cb_num) {
-		return communityMapper.getMember(cb_num);
-	}
-
-	// 클럽 조회
-	public List<Club> getClub(Club cb_num) {
-		return communityMapper.getClub(cb_num);
-	}
-
-	// 클럽 가입
-	public void signClub(Club_Join cb_join) {
-		communityMapper.signClub(cb_join);
-	}
-
+	
 }
