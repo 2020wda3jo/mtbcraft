a0af6e4a83a19155635afb274116173bbc07756f
2020wda3jo <2020wda3jo@github.com>
2020-07-15 11:15:39 +0900

    Revert "Revert "웹 SNS, 마이페이지 기능 추가""
    This reverts commit ab687776380f2124f5c5fbb386cc560ce09cc6a6.

---
+++ spring/mtbcraft/java/com/mtbcraft/service/MyPageService.java
@@ -0,0 +1,37 @@
+package com.mtbcraft.service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mtbcraft.dto.Badge;
+import com.mtbcraft.dto.Course_Review;
+import com.mtbcraft.dto.DangerousArea;
+import com.mtbcraft.dto.Like_Status;
+import com.mtbcraft.dto.RidingRecord;
+import com.mtbcraft.mapper.MyPageMapper;
+import com.mtbcraft.mapper.RidingMapper;
+
+@Service
+@Transactional
+public class MyPageService {
+	@Resource(name="com.mtbcraft.mapper.MyPageMapper")
+	@Autowired
+	private MyPageMapper myPageMapper;
+	
+	//사용자 주행기록 조회
+	public List<RidingRecord> getRR(String rider){
+		return myPageMapper.getRR(rider);
+	}
+	
+	//배지 조회
+	public List<Badge> getBadge(String rider) throws Exception{
+		return myPageMapper.getBadge(rider);
+	}
+	
+}
