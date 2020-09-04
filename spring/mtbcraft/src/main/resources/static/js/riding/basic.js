97eed913ce91db48c5b03e5403669a7c039ffe45
hyungtack <39120185+hyungtack@users.noreply.github.com>
2020-09-04 22:58:37 +0900

    위험지역마커 수정

--- spring/mtbcraft/src/main/resources/static/js/riding/basic.js
+++ spring/mtbcraft/src/main/resources/static/js/riding/basic.js
@@ -7,9 +7,9 @@ $(document).ready(function() {
 
 
 
-var imageSrc = '/imgs/danger.png', // 마커이미지의 주소입니다    
-    imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
-    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+var imageSrc = '/imgs/newdangerPoint.png', // 마커이미지의 주소입니다    
+    imageSize = new kakao.maps.Size(30, 35), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(15, 35)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
     
 var imageSrc2 = '/imgs/noin.png', // 마커이미지의 주소입니다    
